9f1bf820511632b3c7f096cf8893af387e59ee9d
Neven <nrecchia@umass.edu>
2015-12-15 01:39:31 -0500

    redux

--- tabletProject/www/js/app.js
+++ tabletProject/www/js/app.js
@@ -1,13 +1,16 @@
 // Ionic Starter App
 
+// database is global
+var db = null;
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
+angular.module('app', ['ionic', 'ngCordova', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSQLite) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,5 +21,13 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    if(window.cordova){
+      // app syntax
+      db = $cordovaSQLite.openDB("my.db");
+    } else {
+      // ionic serve syntax
+      db = window.openDatabase("my.db", "1.0", "starter", -1)
+    }
+    $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS restaurant (id integer primary key, name text)");
   });
 })
